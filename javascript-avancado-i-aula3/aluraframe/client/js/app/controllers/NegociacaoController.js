03b2f61cb68214c4d71a00fdeb0795045423f68c
Luiz Evangelista <luiz.evangelista@c6bank.com>
2020-04-15 12:51:51 -0300

    adicionando arrow function

--- javascript-avancado-i-aula3/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula3/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -15,10 +15,9 @@ class NegociacaoController {
 
         let data = new Date(...this._inputData
             .value.split('-')
-            .map(function(item, indice) {
-                return item - indice % 2;
-            }));
-
+            .map((item, indice) => item - indice % 2)
+        );
+            
         console.log(data);
 
         let negociacao = new Negociacao(
